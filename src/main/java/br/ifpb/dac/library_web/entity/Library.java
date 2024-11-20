0e6465b56442ca21bb4187f104019fd62f2ee2f6
João Rosivaldo Gomes <joaorgomesoficial@gmail.com>
2024-11-20 20:54:01 -0300

    feature(Adicionando a entidade Library)

---
+++ src/main/java/br/ifpb/dac/library_web/entity/Library.java
@@ -0,0 +1,33 @@
+package br.ifpb.dac.library_web.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_libraries")
+public class Library implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO )
+    @Column(name = "id")
+    private int id;
+
+    @NotBlank(message = "The name cannot be empty")
+    @Column(name = "name")
+    private String name;
+
+
+    @OneToOne(mappedBy = "library", cascade = CascadeType.ALL)
+    private Adress adress;
+
+
+
+}
