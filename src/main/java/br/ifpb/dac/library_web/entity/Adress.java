d132b3376b23825ba23125ead832afd120983db3
AugustoLBA <127113107+AugustoLBA@users.noreply.github.com>
2024-11-20 20:07:50 -0300

    feature(Adicionando as entidades User e Adress)

---
+++ src/main/java/br/ifpb/dac/library_web/entity/Adress.java
@@ -0,0 +1,11 @@
+package br.ifpb.dac.library_web.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "adresses")
+public class Adress implements Serializable {
+}
---
+++ src/main/java/br/ifpb/dac/library_web/entity/User.java
@@ -0,0 +1,41 @@
+package br.ifpb.dac.library_web.entity;
+
+import br.ifpb.dac.library_web.enumeration.TypeUser;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank(message = "The name cannot be empty")
+    @Column(name = "name")
+    private String name;
+
+    @NotBlank(message = "Email cannot be empty")
+    @Column(name = "email")
+    private String email;
+
+    @NotBlank(message = "Password cannot be empty")
+    @Column(name = "password")
+    private String password;
+
+    @NotNull(message = "Type user cannot be empty")
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type_user")
+    private TypeUser type;
+}
