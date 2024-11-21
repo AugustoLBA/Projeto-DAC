b29fee41231b50242ae3fb9a720f8a7256bdec8b
João Rosivaldo Gomes <joaorgomesoficial@gmail.com>
2024-11-20 21:18:29 -0300

    feature(adicionando entidade autor)

---
+++ src/main/java/br/ifpb/dac/library_web/entity/Author.java
@@ -0,0 +1,34 @@
+package br.ifpb.dac.library_web.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+
+import java.io.Serializable;
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "tb_author")
+public class Author implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank(message = "The name cannot be empty")
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "authors")  // "mappedBy" faz referência ao atributo "authors" na classe Book
+    private Set<Book> books;
+
+}
