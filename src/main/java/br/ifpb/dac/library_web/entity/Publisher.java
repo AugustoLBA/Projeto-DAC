f38dad56789d5809c5b11f2df0870b5b84614f1c
AugustoLBA <127113107+AugustoLBA@users.noreply.github.com>
2024-11-20 21:11:40 -0300

    feature(Adicionando a entidade Publisher)

---
+++ src/main/java/br/ifpb/dac/library_web/entity/Publisher.java
@@ -0,0 +1,31 @@
+package br.ifpb.dac.library_web.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_publishers")
+public class Publisher implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @NotBlank(message = "The name field cannot be empty")
+    @Column(name = "name")
+    private String name;
+
+    // Relacionamento OneToMany: Uma editora pode ter vários livros.
+    @OneToMany(mappedBy = "publisher")  // mappedBy define o lado inverso da relação
+    private List<Book> books;
+}
