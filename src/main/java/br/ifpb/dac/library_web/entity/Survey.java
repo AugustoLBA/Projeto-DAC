d3bb0f72cba586018238f9113229fb3b379d0711
João Rosivaldo Gomes <joaorgomesoficial@gmail.com>
2024-11-24 11:30:22 -0300

    Update(Atualizando tipo do Campo id(int) para Long )

--- src/main/java/br/ifpb/dac/library_web/entity/Survey.java
+++ src/main/java/br/ifpb/dac/library_web/entity/Survey.java
@@ -3,11 +3,13 @@ package br.ifpb.dac.library_web.entity;
 import br.ifpb.dac.library_web.enumeration.StatusSurvey;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -25,16 +27,17 @@ public class Survey implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
-    private int id;
+    private Long id;
 
     @Column(name = "date_performed")
-    private Date date_performed;
+    private LocalDateTime date_performed;
 
     @NotBlank(message = "Status cannot be empty")
     @Column(name = "status")
     private StatusSurvey statusSurvey;//definir enum para o status da vistoria
 
     @ManyToOne
+    @NotNull(message = "the exemplary_id field cannot be empty")
     @JoinColumn(name = "exemplary_id") // Define a chave estrangeira para Exemplary
     private Exemplary exemplary;
 }
