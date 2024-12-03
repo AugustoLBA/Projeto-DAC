b87b8ba6231980d655bbc89a971678e6932dc559
João Rosivaldo Gomes <joaorgomesoficial@gmail.com>
2024-12-02 21:41:19 -0300

    update(adicionando metodo de busca por email ao UserRepository)

--- src/main/java/br/ifpb/dac/library_web/repository/UserRepository.java
+++ src/main/java/br/ifpb/dac/library_web/repository/UserRepository.java
@@ -3,5 +3,16 @@ package br.ifpb.dac.library_web.repository;
 import br.ifpb.dac.library_web.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    /**
+     * Metodo adicional para buscar um Usuario por email
+     * @param email
+     * @return O método findByEmail retorna um Optional<User> porque o email pode não existir no banco de dados.
+     */
+    Optional<User> findByEmail(String email);
+
 }
+
