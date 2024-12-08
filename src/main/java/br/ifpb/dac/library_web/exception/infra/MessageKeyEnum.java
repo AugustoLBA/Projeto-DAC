f32ba2642eba1e238622a7edddcf7711e9ff55b6
João Rosivaldo Gomes <joaorgomesoficial@gmail.com>
2024-12-08 20:26:26 -0300

    Update(Adicionando mensagens personalizadas para erros survey)

--- src/main/java/br/ifpb/dac/library_web/exception/infra/MessageKeyEnum.java
+++ src/main/java/br/ifpb/dac/library_web/exception/infra/MessageKeyEnum.java
@@ -7,7 +7,10 @@ public enum MessageKeyEnum {
 
     USER_NOT_FOUND_WITH_ID("User not found with ID:"),
     USER_NOT_FOUND_WITH_EMAIL("User not found with email:"),
-    INVALID_FIELDS("Invalid field(s)");
+    INVALID_FIELDS("Invalid field(s)"),
+    SURVEY_NOT_FOUND_WITH_ID("Survey not found with ID:"),
+    SURVEY_NOT_FOUND_WITH_EMAIL("Survey not found with email:");
+
     private String message;
 
     MessageKeyEnum(String message) {
