8ba07fb486b5f6eb6493734f0911a82153ba7fc7
AugustoLBA <127113107+AugustoLBA@users.noreply.github.com>
2024-12-08 21:33:14 -0300

    feature(Adicioando mais mensagens ao enum)

--- src/main/java/br/ifpb/dac/library_web/exception/infra/MessageKeyEnum.java
+++ src/main/java/br/ifpb/dac/library_web/exception/infra/MessageKeyEnum.java
@@ -1,16 +1,21 @@
 package br.ifpb.dac.library_web.exception.infra;
 
 import lombok.Getter;
-
 @Getter
 public enum MessageKeyEnum {
 
     USER_NOT_FOUND_WITH_ID("User not found with ID:"),
     USER_NOT_FOUND_WITH_EMAIL("User not found with email:"),
-    INVALID_FIELDS("Invalid field(s)");
+    INVALID_FIELDS("Invalid field(s)"),
+    THE_NAME_CANNOT_BE_EMPTY("The name cannot be empty"),
+    THE_EMAIL_CANNOT_BE_EMPTY("The e-mail cannot be empty"),
+    THE_PASSWORD_CANNOT_BE_EMPTY("The password cannot be empty"),
+    THE_TYPER_USER_CANNOT_BE_EMPTY("The typer user cannot be empty"),
+    THE_EMAIL_FORMAT_IS_INVALID("The e-mail format is invalid");
     private String message;
 
     MessageKeyEnum(String message) {
+        this.message = message;
     }
 
     public String getMessage(Object aux){
