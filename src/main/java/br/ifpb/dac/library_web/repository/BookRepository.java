37aaa3349710c152a271ac8716435deae319be7c
João Rosivaldo Gomes <joaorgomesoficial@gmail.com>
2024-12-10 20:15:36 -0300

    Update(adcionando metodos de busca por titulo e por autor para implementação em bookService )

--- src/main/java/br/ifpb/dac/library_web/repository/BookRepository.java
+++ src/main/java/br/ifpb/dac/library_web/repository/BookRepository.java
@@ -3,5 +3,10 @@ package br.ifpb.dac.library_web.repository;
 import br.ifpb.dac.library_web.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByTitle(String title);
+    List<Book> findByAuthor(String author);
 }
