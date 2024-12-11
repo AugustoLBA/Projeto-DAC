ddb0c32f079bedd6370cd01cebcc55a11444e818
João Rosivaldo Gomes <joaorgomesoficial@gmail.com>
2024-12-11 13:00:08 -0300

    feature(Adicionando book response )

---
+++ src/main/java/br/ifpb/dac/library_web/dto/BookResponse.java
@@ -0,0 +1,25 @@
+package br.ifpb.dac.library_web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Map;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookResponse {
+
+    private Long id;
+    private String title;
+    private String isbn;
+    private Integer yearPublication;
+    private Integer numberPager;
+    private String publisherName;
+    private Map<String,Integer> chapters;
+    private List<String> authorNames;
+
+}
