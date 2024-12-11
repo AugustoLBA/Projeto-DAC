e76131320fecb2e0fe1e29d1612d85ee8496ffca
João Rosivaldo Gomes <joaorgomesoficial@gmail.com>
2024-12-11 13:03:18 -0300

    update(adcioando query personalisada )

--- src/main/java/br/ifpb/dac/library_web/repository/BookRepository.java
+++ src/main/java/br/ifpb/dac/library_web/repository/BookRepository.java
@@ -1,12 +1,16 @@
 package br.ifpb.dac.library_web.repository;
 
+import br.ifpb.dac.library_web.entity.Author;
 import br.ifpb.dac.library_web.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
     Optional<Book> findByTitle(String title);
-    List<Book> findByAuthor(String author);
+
+    @Query("SELECT b FROM Book b JOIN b.authors a WHERE a.name = ?1")
+    List<Book> findBooksByAuthors(String authorName);
 }
