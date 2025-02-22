1b78ad138e962cbbbddee95109d0c1795bacd8ac
João Rosivaldo Gomes <joaorgomesoficial@gmail.com>
2025-02-21 23:39:26 -0300

    update(alteração de mapper)

--- src/main/java/br/ifpb/dac/library_web/mapper/BookMapper.java
+++ src/main/java/br/ifpb/dac/library_web/mapper/BookMapper.java
@@ -6,30 +6,38 @@ import br.ifpb.dac.library_web.entity.Author;
 import br.ifpb.dac.library_web.entity.Book;
 import br.ifpb.dac.library_web.entity.Publisher;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.PropertyMap;
 
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
-import static org.hibernate.Hibernate.map;
-
 public class BookMapper {
+    private static final ModelMapper mapper = new ModelMapper();
+
     public static Book toBook(BookRequest request) {
-        ModelMapper mapper = new ModelMapper();
+        if (request == null) {
+            return null;
+        }
+
         Book book = mapper.map(request, Book.class);
 
-        if (request.getPublisherName() != null) {
+        // Mapeamento do Publisher
+        if (request.getPublisherId() != null) {
             Publisher publisher = new Publisher();
-            publisher.setName(request.getPublisherName());
+            publisher.setId(request.getPublisherId());
             book.setPublisher(publisher);
         }
 
+        // Mapeamento dos Chapters
+        if (request.getChapters() != null) {
+            book.setChapters(request.getChapters());
+        }
+
+        // Mapeamento dos Authors
         if (request.getAuthorIds() != null) {
             List<Author> authors = request.getAuthorIds().stream()
-                    .map(name -> {
+                    .map(id -> {
                         Author author = new Author();
-                        author.setName(author.getName());
+                        author.setId(id); // Define apenas o ID do autor
                         return author;
                     })
                     .collect(Collectors.toList());
@@ -40,25 +48,21 @@ public class BookMapper {
     }
 
     public static BookResponse toBookResponse(Book book) {
-        /*PropertyMap<Book, BookResponse> props = new PropertyMap<Book, BookResponse>() {
-            @Override
-            protected void configure() {
-                map().setPublisherName(source.getPublisher() != null ? source.getPublisher().getName() : null);
-                map().setAuthorNames(source.getAuthors().stream().map(Author::getName).toList());
-            }
-        };
-        ModelMapper mapper = new ModelMapper();
-        mapper.addMappings(props);
-        return mapper.map(book, BookResponse.class);*/
+        if (book == null) {
+            return null;
+        }
 
-        BookResponse bookResponse = new BookResponse();
-        bookResponse.setId(book.getId());
-        bookResponse.setTitle(book.getTitle());
-        bookResponse.setIsbn(book.getIsbn());
-        bookResponse.setChapters(book.getChapters());
-        bookResponse.setPublisherName(book.getPublisher().getName());
-        bookResponse.setYearPublication(book.getYearPublication());
-        bookResponse.setAuthorNames(book.getAuthors().stream().map(Author::getName).collect(Collectors.toList()));
+        BookResponse bookResponse = mapper.map(book, BookResponse.class);
+
+        // Mapeamento manual do nome do Publisher
+        if (book.getPublisher() != null) {
+            bookResponse.setPublisherName(book.getPublisher().getName());
+        }
+
+        // Mapeamento manual dos nomes dos Authors
+        if (book.getAuthors() != null) {
+            bookResponse.setAuthorNames(book.getAuthors()); // Aqui passamos a lista de autores diretamente
+        }
 
         return bookResponse;
 
